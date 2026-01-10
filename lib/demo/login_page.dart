a9f6063fb5c25df73e8ad914a33dc6d4717499e0
gustiarifiyanto <gusti.arifiyanto@gmail.com>
2026-01-10 11:16:24 +0700

    Login page done practice

--- lib/demo/login_page.dart
+++ lib/demo/login_page.dart
@@ -11,6 +11,7 @@ class _LoginPageState extends State<LoginPage> {
   final formKey = GlobalKey<FormState>();
 
   final usernameController = TextEditingController();
+  final emailController = TextEditingController();
   final passwordController = TextEditingController();
 
   bool isObscure = true;
@@ -24,34 +25,64 @@ class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: ListView(
-        padding: EdgeInsets.all(16),
-        children: [
-          SizedBox(height: 120),
-          Image.asset('assets/images/Main.png'),
-          TextFormField(
-            controller: usernameController,
-            decoration: InputDecoration(label: Text('Username')),
-          ),
-          SizedBox(height: 20),
-          TextFormField(
-            controller: passwordController,
-            obscureText: isObscure,
-            decoration: InputDecoration(
-              label: Text('Password'),
-              suffixIcon: GestureDetector(
-                onTap: () {
-                  toggleObscure();
-                },
-                child: Icon(
-                  isObscure ? Icons.visibility_off : Icons.visibility,
+      body: Form(
+        key: formKey,
+        child: ListView(
+          padding: EdgeInsets.all(16),
+          children: [
+            SizedBox(height: 120),
+            Image.asset('assets/images/Main.png'),
+            TextFormField(
+              controller: usernameController,
+              decoration: InputDecoration(label: Text('Username')),
+              validator: (value) {
+                if (value!.isEmpty) {
+                  return 'Username Tidak Boleh Kosong';
+                }
+                return null;
+              },
+            ),
+            SizedBox(height: 20),
+            TextFormField(
+              controller: emailController,
+              decoration: InputDecoration(label: Text('Email')),
+              validator: (value) {
+                if (value!.isEmpty) {
+                  return 'Email Tidak Boleh Kosong';
+                }
+                return null;
+              },
+            ),
+            TextFormField(
+              controller: passwordController,
+              obscureText: isObscure,
+              decoration: InputDecoration(
+                label: Text('Password'),
+                suffixIcon: GestureDetector(
+                  onTap: () {
+                    toggleObscure();
+                  },
+                  child: Icon(
+                    isObscure ? Icons.visibility_off : Icons.visibility,
+                  ),
                 ),
               ),
+              validator: (value) {
+                if (value!.isEmpty) {
+                  return 'Password Tidak Boleh Kosong';
+                }
+                return null;
+              },
             ),
-          ),
-          SizedBox(height: 20),
-          ElevatedButton(onPressed: () {}, child: Text('Login')),
-        ],
+            SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () {
+                if (formKey.currentState!.validate()) {}
+              },
+              child: Text('Login'),
+            ),
+          ],
+        ),
       ),
     );
   }
